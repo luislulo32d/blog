706abf3704a39fbf753990c0f580a4088156fa7c
Kai Luis Díaz <luislulo32d@gmail.com>
2024-04-25 21:38:39 -0400

    first view

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $articles = Article::where('status', '1')
+        ->orderBy('id', 'desc')
+        ->simplePaginate(10);
+        
+
+        $navbar = Category::where([
+            ['status', '1'],
+            ['is_featured', '1'],
+        ])->paginate(3);
+
+        return view('home', compact('articles', 'navbar'));
+    }
+    public function all(){
+        $categories = Category::where('status', '1')
+            ->simplePaginate(20);
+        $navbar = Category::where([
+            ['status', '1'],
+            ['is_featured', '1'],
+        ])->paginate(3);
+
+        return view('home.all-categories', compact('categories','navbar'));
+    }
+}
---
+++ app/Notifications/WelcomeEmail.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeEmail extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->greeting('Hola, Bienvenido al blog!')
+                    ->line('Nos complace darte la bienvenida a nuestro blog, 
+                    ahora que estás registrado puedes realizar comentarios 
+                    en los articulos del blog.')
+                    ->action('Ir al blog', url('/'))
+                    ->line('Gracias por ser parte de nuestra comunidad!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
